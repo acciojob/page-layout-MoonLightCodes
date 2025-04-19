3c055ff7324e98d60fe73859234069e8624b9850
PoornachanderReddy <poornachander295@gmail.com>
2025-04-20 01:33:19 +0530

    updated and using single component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "./../styles/App.css";
-import { Header } from "./Header";
-import { Child } from "./Child";
-import { Footer } from "./Footer";
-
+import PageLayout from "./PageLayour";
 const App = () => {
   return (
     <>
-      <Header />
-       <Child />
-      <Footer />
+      <PageLayout
+        header={<h1>Welcome to My Page</h1>}
+        footer={<h3>© {new Date().getFullYear()} MyWebsite</h3>}
+        children={<h3>This is the main content of the page.</h3>}
+      ></PageLayout>
     </>
   );
 };
