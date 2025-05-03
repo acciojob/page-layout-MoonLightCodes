e1ec401a5d54077de1292879bed7e3a01c285d73
PoornachanderReddy <poornachander295@gmail.com>
2025-05-03 10:51:12 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,10 @@ const App = () => {
   return (
     <>
       <PageLayout
-      header={<h1>Welcome to My Page</h1>}
-      footer={<h3>© {new Date().getFullYear()} MyWebsite</h3>}
+      header={<h1>Welcome to my website</h1>}
+      footer={<h3>© {new Date().getFullYear()} My Website. All rights reserved</h3>}
     >
-      <h3>This is the main content of the page.</h3>
+      <h3>This is the content of the page.</h3>
     </PageLayout>
     </>
   );
