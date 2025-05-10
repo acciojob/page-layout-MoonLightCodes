140be28e86a3b877fa2cd152843a7ced10199e34
PoornachanderReddy <poornachander295@gmail.com>
2025-05-10 10:52:14 +0530

    complted hope

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,14 @@ import "./../styles/App.css";
 import PageLayout from "./PageLayour";
 const App = () => {
   return (
-    <>
-      <PageLayout
+    <PageLayout
       header={<h1>Welcome to my website</h1>}
-      footer={<h3>© {new Date().getFullYear()} My Website. All rights reserved</h3>}
+      footer={
+        <h3>© {new Date().getFullYear()} My Website. All rights reserved</h3>
+      }
     >
       <h3>This is the content of the page.</h3>
     </PageLayout>
-    </>
   );
 };
 
